8f4c8647b817b0894a803ad344c0bec3618cdf74
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-30 01:45:14 +0000

    complete!

--- dice.rb
+++ dice.rb
@@ -11,6 +11,12 @@ get("/") do
     <li>
       <a href=\"/dice/2/10\">Roll two 10-sided dice</a>
     </li>
+    <li>
+      <a href=\"/dice/1/20\">Roll one 20-sided dice</a>
+    </li>
+    <li>
+      <a href=\"/dice/60/6\">Roll 60 6-sided dice</a>
+    </li>
   </ul>
   "
 end
@@ -52,6 +58,60 @@ get("/dice/2/10") do
 
   "
   <h1>2d10</h1>
+  <h2>#{outcome}</h2>
+  <p><a href=\"/\">Return to homepage</p>
+  "
+end
+
+get("/dice/1/20") do
+  number_rolls = 1
+  dice_roll = Array.new
+  outcome = String.new
+  counter = 0 # used for the outcome loop
+  
+  
+  while dice_roll.length < number_rolls
+    dice_roll.push(rand(1..20))
+  end
+
+  # outcome loop
+  dice_roll.each do |the_roll|
+    counter = counter + 1
+    outcome = outcome + "Roll # #{counter}: #{the_roll}<br>"
+  end
+  outcome = outcome + "Total = #{dice_roll.sum}"
+
+  # "Test message"
+
+  "
+  <h1>1d20</h1>
+  <h2>#{outcome}</h2>
+  <p><a href=\"/\">Return to homepage</p>
+  "
+end
+
+get("/dice/60/6") do
+  number_rolls = 60
+  dice_roll = Array.new
+  outcome = String.new
+  counter = 0 # used for the outcome loop
+  
+  
+  while dice_roll.length < number_rolls
+    dice_roll.push(rand(1..6))
+  end
+
+  # outcome loop
+  dice_roll.each do |the_roll|
+    counter = counter + 1
+    outcome = outcome + "Roll # #{counter}: #{the_roll}<br>"
+  end
+  outcome = outcome + "Total = #{dice_roll.sum}"
+
+  # "Test message"
+
+  "
+  <h1>60d6</h1>
   <h2>#{outcome}</h2>
   <p><a href=\"/\">Return to homepage</p>
   "
