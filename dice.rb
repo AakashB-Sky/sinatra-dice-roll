bd7bc2d79a60382930eac96bbb5eb921be061bda
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-30 01:19:26 +0000

    2d6 working

--- dice.rb
+++ dice.rb
@@ -1,13 +1,21 @@
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
   "Hello World!"
 end
 
-get("/zebra") do
-  "We must add a route for each path that we support"
+get("/test") do
+  "This is a test message."
 end
 
-get("/test") do
-  "Hopefully this shows up without having to restart the server"
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d6</h>
+  <p>#{outcome}</p>"
 end
