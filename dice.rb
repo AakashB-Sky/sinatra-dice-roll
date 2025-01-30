64d751b0d6b339858c042f49f2db5520eb43892e
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-30 01:26:52 +0000

    basic hyperlinks set up

--- dice.rb
+++ dice.rb
@@ -2,12 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "Hello World!"
+  "
+  <h1>Let's roll some dice!
+  <ul>
+    <li>
+      <a href=\"/dice/2/6\">Roll two 6-sided dice</a>
+    </li>
+    <li>
+      Test
+    </li>
+  </ul>
+  "
 end
 
-get("/test") do
-  "This is a test message."
-end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
@@ -16,6 +23,9 @@ get("/dice/2/6") do
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h>
-  <p>#{outcome}</p>"
+  "
+  <h1>2d6</h1>
+  <h2>#{outcome}</h2>
+  <p><a href=\"/\">Return to homepage</a></p>
+  "
 end
